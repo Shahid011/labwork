9bd2d6b62b2787a0d85f39032364974cc084042f
shahid alam <shahid.alam3465@gmail.com>
2021-11-29 12:35:04 +0545

    second commit

---
+++ lab1.py
@@ -0,0 +1,7 @@
+input_1st= int(input("Write your first input: "))
+input_2st= int(input("Write your second input: "))
+input_3st= int(input("Write your third input: "))
+
+sum_all = input_1st+input_2st+input_3st
+
+print("This is the sum of all three inputs: ", sum_all)

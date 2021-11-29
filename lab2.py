898420f136c580447e9516e4809e050cd5abd00d
shahid alam <shahid.alam3465@gmail.com>
2021-11-29 12:58:30 +0545

    secondary commit

---
+++ lab2.py
@@ -0,0 +1,6 @@
+base = float(input("Enter the base of a triangle: "))
+height = float(input("Enter the height of a triangle: "))
+
+area_of_triangle = 0.5 * base * height
+
+print(f"Area of triangle is: {area_of_triangle} cm\u00b2")
---
+++ lab3.py
@@ -0,0 +1,5 @@
+length = float(input("Enter the lenght of cube: "))
+
+volume  = length**3
+
+print(f"volume of cube: {volume} cm\u00b3")

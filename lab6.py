1c197c8a5d3288ec9d467422071d341e795005bc
shahid alam <shahid.alam3465@gmail.com>
2021-12-01 18:39:59 +0545

    home assignment commit

--- lab6.py
+++ lab6.py
@@ -1,19 +1,8 @@
-import pdb
-bike1 = 'yahama'
-bike1_price = 25000
+num = int(input("print sum of even numbers till: "))
+sum = 0
 
-bike2 = 'honda'
-bike2_price = 50000
+for i in range(1, num+1):
+    if(i % 2 == 0):
+        sum += i
 
-pdb.set_trace()
-
-name = input("Enter your name: ")
-choose = input('Press 1 for yahama and 2 for honda: ')
-
-if choose == 1:
-    print(f"{bike1} will cost you {bike1_price}")
-elif choose == 2:
-    print(f"{bike2 + 20000} will cost you {bike2_price}")
-
-else:
-    print("Press only 1 or 2")
+print(f"The sum of first {num} even number is {sum}")
--- lab7.py
+++ lab7.py
@@ -1,1 +1,8 @@
-print("sum of first 10 odd numbers")
+num = int(input("print sum of odd numbers till: "))
+sum = 0
+
+for i in range(1, num+1):
+    if(not(i%2)==0):
+        sum += i
+
+print(f"The sum of first {num} odd number is {sum}")
